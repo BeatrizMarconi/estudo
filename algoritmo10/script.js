a03cf349bb618eab33e48b46122e355cf05cd427
Beatriz Marconi <beatrizmarconi.dev@gmail.com>
2021-12-03 17:16:03 -0300

    concatenando variaveis

--- algoritmo10/script.js
+++ algoritmo10/script.js
@@ -6,8 +6,16 @@
 
 // console.log(`sua média é ${res}`)
 
-let num1=15;
-let num2=35;
-let res= num1 + num2;
 
-console.log(`O resultado da soma das variáveis é: ${res}`)
+// let num1=15;
+// let num2=35;
+// let res= num1 + num2;
+
+// console.log(`O resultado da soma das variáveis é: ${res}`)
+
+
+let nome = 'cheová';
+let sobrenome = 'marconi';
+let idade = 34;
+
+console.log(`Olá! Meu nome é ${nome} ${sobrenome} e eu tenho ${idade} anos`)
