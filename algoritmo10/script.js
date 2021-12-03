d10a7b74d4982fccddc9b3899b196c9704b6ccda
Beatriz Marconi <beatrizmarconi.dev@gmail.com>
2021-12-03 17:39:59 -0300

    condicionais

--- algoritmo10/script.js
+++ algoritmo10/script.js
@@ -1,11 +1,14 @@
-// let nota_1= 10;
-// let nota_2= 5;
-// let nota_3= 7;
-// let nota_4= 7;
-// let res= (nota_1 + nota_2 + nota_3 + nota_4)/4;
+let nota_1= 10;
+let nota_2= 5;
+let nota_3= 7;
+let nota_4= 7;
+let media= (nota_1 + nota_2 + nota_3 + nota_4)/4;
 
-// console.log(`sua média é ${res}`)
-
+if(media >= 6){
+    console.log(`APROVADO, sua média é: ${media}`)
+}else{
+    console.log(`REPROVADO, sua média é: ${media}`)
+}
 
 // let num1=15;
 // let num2=35;
@@ -14,8 +17,9 @@
 // console.log(`O resultado da soma das variáveis é: ${res}`)
 
 
-let nome = 'cheová';
-let sobrenome = 'marconi';
-let idade = 34;
+// let nome = 'cheová';
+// let sobrenome = 'marconi';
+// let idade = 34;
 
-console.log(`Olá! Meu nome é ${nome} ${sobrenome} e eu tenho ${idade} anos`)
+// console.log(`Olá! Meu nome é ${nome} ${sobrenome} e eu tenho ${idade} anos`)
+
