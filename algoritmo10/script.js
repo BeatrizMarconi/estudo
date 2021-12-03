53b6fb04e43f6f48ad4c8d278c864a883386eb98
Beatriz Marconi <beatrizmarconi.dev@gmail.com>
2021-12-03 17:52:40 -0300

    condicionais else/if

--- algoritmo10/script.js
+++ algoritmo10/script.js
@@ -1,3 +1,15 @@
+// let nota_1= 10;
+// let nota_2= 5;
+// let nota_3= 7;
+// let nota_4= 7;
+// let media= (nota_1 + nota_2 + nota_3 + nota_4)/4;
+
+// if(media >= 6){
+//     console.log(`APROVADO, sua média é: ${media}`)
+// }else{
+//     console.log(`REPROVADO, sua média é: ${media}`)
+// }
+
 let nota_1= 10;
 let nota_2= 5;
 let nota_3= 7;
@@ -5,10 +17,16 @@ let nota_4= 7;
 let media= (nota_1 + nota_2 + nota_3 + nota_4)/4;
 
 if(media >= 6){
-    console.log(`APROVADO, sua média é: ${media}`)
-}else{
-    console.log(`REPROVADO, sua média é: ${media}`)
+    console.log(`Aluno aprovado com média ${media}`)
 }
+else if(media >= 5 && media <= 5.9){
+    console.log(`Aluno será encaminhado para conselho de classe`)
+}
+else{
+    console.log(`Aluno reprovado com média ${media}`)
+}
+
+
 
 // let num1=15;
 // let num2=35;
