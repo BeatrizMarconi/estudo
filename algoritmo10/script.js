624c1dc2267daf348d0cc6926de32c1a060f6086
Beatriz Marconi <beatrizmarconi.dev@gmail.com>
2021-12-03 17:09:46 -0300

    soma das variaveis

--- algoritmo10/script.js
+++ algoritmo10/script.js
@@ -1,7 +1,13 @@
-let nota_1= 10;
-let nota_2= 5;
-let nota_3= 7;
-let nota_4= 7;
-let res= (nota_1 + nota_2 + nota_3 + nota_4)/4;
+// let nota_1= 10;
+// let nota_2= 5;
+// let nota_3= 7;
+// let nota_4= 7;
+// let res= (nota_1 + nota_2 + nota_3 + nota_4)/4;
 
-console.log(`sua média é ${res}`)
+// console.log(`sua média é ${res}`)
+
+let num1=15;
+let num2=35;
+let res= num1 + num2;
+
+console.log(`O resultado da soma das variáveis é: ${res}`)
