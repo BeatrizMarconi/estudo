840563fb576968d2d3cae69c380acb81f52fea86
Beatriz Marconi <beatrizmarconi.dev@gmail.com>
2021-12-08 17:39:57 -0300

    mostrando a terceira fruta

--- arrays/script.js
+++ arrays/script.js
@@ -1,7 +1,11 @@
+// let frutas = ['abacaxi','maçã','manga', 'melão','abacate'];
+
+// console.log(frutas[0]);
+// console.log(frutas[1]);
+// console.log(frutas[2]);
+// console.log(frutas[3]);
+// console.log(frutas[4]);
+
 let frutas = ['abacaxi','maçã','manga', 'melão','abacate'];
 
-console.log(frutas[0]);
-console.log(frutas[1]);
 console.log(frutas[2]);
-console.log(frutas[3]);
-console.log(frutas[4]);
