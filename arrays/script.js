fb8a18c9c635c8ac7749e5b5c6b00656dcf351b4
Beatriz Marconi <beatrizmarconi.dev@gmail.com>
2021-12-08 17:45:43 -0300

    mostrando a primeira e ultima fruta

--- arrays/script.js
+++ arrays/script.js
@@ -6,6 +6,11 @@
 // console.log(frutas[3]);
 // console.log(frutas[4]);
 
+// let frutas = ['abacaxi','maçã','manga', 'melão','abacate'];
+
+// console.log(frutas[2]);
+
 let frutas = ['abacaxi','maçã','manga', 'melão','abacate'];
 
-console.log(frutas[2]);
+console.log(frutas[0]);
+console.log(frutas[frutas.length -1]);
