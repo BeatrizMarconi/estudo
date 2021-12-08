18498d842cec52a554cbd41a35b85a9cb1e91676
Beatriz Marconi <beatrizmarconi.dev@gmail.com>
2021-12-08 17:32:54 -0300

    array frutas

---
+++ arrays/script.js
@@ -0,0 +1,7 @@
+let frutas = ['abacaxi','maçã','manga', 'melão','abacate'];
+
+console.log(frutas[0]);
+console.log(frutas[1]);
+console.log(frutas[2]);
+console.log(frutas[3]);
+console.log(frutas[4]);
