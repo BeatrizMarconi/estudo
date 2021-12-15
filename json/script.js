0758e0db8e5c72377abf246784234121d8e4044c
Beatriz Marconi <beatrizmarconi.dev@gmail.com>
2021-12-15 13:24:22 -0300

    Json carros

---
+++ json/script.js
@@ -0,0 +1,10 @@
+let carros = {
+        "marca":"Renault",
+        "modelo":"Clio",
+        "cor":"Cinza",
+        "motor": 1.0,
+        "ano": 2000,
+        "combustivel":"gasolina"
+    }
+
+console.log(`Meu carro é um ${carros.marca}, ano ${carros.ano}, modelo ${carros.modelo}, cor ${carros.cor}, combustivel ${carros.combustivel}.`)
