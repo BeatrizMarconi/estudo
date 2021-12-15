d00ef4f6834b30785d9aa5a1be93ce250443e630
Beatriz Marconi <beatrizmarconi.dev@gmail.com>
2021-12-15 17:37:31 -0300

    array e for

---
+++ for/script.js
@@ -0,0 +1,5 @@
+let nome = ['Marcia','Roberta','Nicolas', 'Ivis','Eliane','Beatriz','Lucas','Vania','joabe','jair'];
+
+for(let index = 0 ; index < nome.length; index++) {
+    console.log(`Olá meu nome é ${nome[index]}`);
+}
