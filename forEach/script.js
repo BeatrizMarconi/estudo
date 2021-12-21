059338409bcf860f85c4a9e244a9c6b8a7777c3b
Beatriz Marconi <beatrizmarconi.dev@gmail.com>
2021-12-21 13:07:42 -0300

    forEach e Json

--- forEach/script.js
+++ forEach/script.js
@@ -1,5 +1,16 @@
-let nomes = ['Marcia','Roberta','Nicolas', 'Ivis','Eliane','Beatriz','Lucas','Vania','joabe','jair'];
+// let nomes = ['Marcia','Roberta','Nicolas', 'Ivis','Eliane','Beatriz','Lucas','Vania','joabe','jair'];
 
-nomes.forEach(nome => {
-    console.log(`Olá meu nome é ${nome}`)
-})
+// nomes.forEach(nome => {
+//     console.log(`Olá meu nome é ${nome}`)
+// });
+
+let carros = [{
+    "marca":"Renault",
+    "ano": 2000,
+    "modelo":"Clio",
+    "km" : "136.000"
+}]
+
+carros.forEach((carro) => {
+    console.log(`A marca do carro é ${carro.marca}, Modelo ${carro.modelo}, Ano ${carro.ano}, e sua quilometragem é de ${carro.km}.`)
+});
